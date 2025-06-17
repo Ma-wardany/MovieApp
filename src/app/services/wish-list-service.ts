de514ca9884859832d6761322197f5cb3c6c8f1c
Ma-wardany <wardany.dev@gmail.com>
2025-06-17 23:41:36 +0300

    dynamic movie card

--- src/app/services/wish-list-service.ts
+++ src/app/services/wish-list-service.ts
@@ -1,26 +1,25 @@
 import { Injectable } from '@angular/core';
 
 @Injectable({
-  providedIn: 'root'
+  providedIn: 'root',
 })
 export class WishListService {
-
   private movieWishList: any[] = [];
 
-  getWishList()
-  {
+  getWishList() {
     return this.movieWishList;
   }
 
-  addToWishList(movie: any)
-  {
-    if(!this.movieWishList.find(m => m.id == movie.id))
-    {
+  addToWishList(movie: any) {
+    if (!this.movieWishList.find((m) => m.id == movie.id)) {
       this.movieWishList.push(movie);
+      console.log("added");
+
     }
   }
 
-    removeFromWishlist(id: number) {
-    this.movieWishList = this.movieWishList.filter(m => m.id !== id);
+  removeFromWishlist(id: number) {
+    this.movieWishList = this.movieWishList.filter((m) => m.id !== id);
+    console.log("removed");
   }
 }
